efcda6a3e2368a45c8ef85b8a66c3cbdb645bd06
Anant <anantarnav007@gmail.com>
2021-04-24 16:56:46 +0530

    done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,44 +1,55 @@
-import React, { useContext } from 'react'
-import { useQuery } from '@apollo/react-hooks'
-import { Grid, GridColumn, GridRow, Transition } from 'semantic-ui-react'
-import { AuthContext } from '../context/auth'
-import PostForm from '../components/PostForm'
-import { FETCH_POSTS_QUERY } from '../util/graphql'
-import PostCard from '../components/PostCard'
+import React, { useContext, useState, useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { Grid, GridColumn, GridRow, Transition } from "semantic-ui-react";
+import { AuthContext } from "../context/auth";
+import PostForm from "../components/PostForm";
+import { FETCH_POSTS_QUERY } from "../util/graphql";
+import PostCard from "../components/PostCard";
 
 function Home() {
-    const { user } = useContext(AuthContext)
-    const { loading, data } = useQuery(FETCH_POSTS_QUERY)
+  const { user } = useContext(AuthContext);
+  const { loading, data, error } = useQuery(FETCH_POSTS_QUERY);
+  const [allPosts, setAllPosts] = useState([]);
 
-    return (
-        <Grid columns={3}>
-            <GridRow className="page-title">
-                <h1>Recent Posts</h1>
-            </GridRow>
-            <Grid.Row>
-                {user && (
-                    <GridColumn>
-                        <PostForm />
-                    </GridColumn>
-                )}
-                {loading
-                    ? (<h1>Loading Posts</h1>)
-                    : (
-                        <Transition.Group>
-                            {
-                                data.getPosts &&
-                                data.getPosts.map((post) => (
-                                    <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
-                                        <PostCard post={post} />
-                                    </Grid.Column>
-                                ))
-                            }
-                        </Transition.Group>
-                    )
-                }
-            </Grid.Row>
-        </Grid>
-    )
+  const updatePosts = (updatedPosts) => {
+    setAllPosts([...updatedPosts]);
+  };
+
+  useEffect(() => {
+    if (data?.getPosts.length > 0) {
+      setAllPosts(data?.getPosts);
+    }
+  }, [data?.getPosts]);
+
+  if (loading) return <p>LOADING...</p>;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Grid columns={3}>
+      <GridRow className="page-title">
+        <h1>Recent Posts</h1>
+      </GridRow>
+      <Grid.Row>
+        {user && (
+          <GridColumn>
+            <PostForm updatePosts={updatePosts} />
+          </GridColumn>
+        )}
+        {loading ? (
+          <h1>Loading Posts</h1>
+        ) : (
+          <Transition.Group>
+            {allPosts.map((post) => (
+              <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                <PostCard post={post} />
+              </Grid.Column>
+            ))}
+          </Transition.Group>
+        )}
+      </Grid.Row>
+    </Grid>
+  );
 }
 
-export default Home
+export default Home;
--- client/src/util/hooks.js
+++ client/src/util/hooks.js
@@ -1,20 +1,21 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 export const useForm = (callback, initialState = {}) => {
-    const [values, setValues] = useState(initialState)
+  const [values, setValues] = useState(initialState);
 
-    const onChange = (event) => {
-        setValues({ ...values, [event.target.name]: event.target.value });
-    }
+  const onChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        callback();
-    }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (values.body.length > 0) callback();
+    else alert("Please enter some message");
+  };
 
-    return {
-        onChange,
-        onSubmit,
-        values
-    }
-}
+  return {
+    onChange,
+    onSubmit,
+    values,
+  };
+};
